d1e8e4886e3aeaf73425b736efba4acd9fa3d11b
keerthirajen11 <138966226+keerthirajen11@users.noreply.github.com>
2023-07-08 22:16:09 +0530

    Add files via upload

---
+++ sql files.sql
@@ -0,0 +1,9 @@
+create database parimala;
+use parimala;
+create Table Login(username varchar(30),password varchar(20));
+select * from Login;
+create Table Product(ProductId int,ProductName varchar(30),minsellQuantity int,price int,quantity int);
+insert into Login values('harsh','pari');
+desc product;
+select * from product;
+ 
